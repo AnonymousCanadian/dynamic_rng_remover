0f8b61235adfd7cd3c6ffd20821cd0dc6e5eadf1
AnonymousCanadian <CanadianCatboy@proton.me>
2025-06-11 18:42:16 -0700

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -212,8 +212,7 @@ window.disableGrayMode = function() {
 };
 
   window.changeTab = function(newTab, tabId, isRight) {
-      if (tabId == 'poll_tab' && (dendryUI.dendryEngine.state.qualities.historical_mode || dendryUI.dendryEngine.state.qualities.rubicon)) {
-          if (dendryUI.dendryEngine.state.qualities.historical_mode && !dendryUI.dendryEngine.state.qualities.rubicon) window.alert('Polls are not available in historical mode.');
+      if (tabId == 'poll_tab' && (dendryUI.dendryEngine.state.qualities.rubicon)) {
           if (dendryUI.dendryEngine.state.qualities.rubicon) window.alert('Polls are not available after crossing the rubicon.');
           return;
       }
